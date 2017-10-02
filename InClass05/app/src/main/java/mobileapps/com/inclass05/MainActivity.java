00ddf8cfaea25a7a22a57c25955aad369b9c31b2
Jeremy Bohannon <jeremybohannon@outlook.com>
2017-10-02 19:22:58 -0400

    Starter

--- InClass05/app/src/main/java/mobileapps/com/inclass05/MainActivity.java
+++ InClass05/app/src/main/java/mobileapps/com/inclass05/MainActivity.java
@@ -3,11 +3,56 @@ package mobileapps.com.inclass05;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String response = makeConnection("http://dev.theappsdr.com/apis/photos/keywords.php");
+
+
+    }
+
+    String makeConnection(String requestedURL){
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        String result = null;
+        try {
+            URL url = new URL(requestedURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                result = stringBuilder.toString();
+            }
+        } catch (Exception e) {
+            //Handle the exceptions
+        } finally {
+            //Close open connections and reader
+            if (connection != null) {
+                connection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 }
