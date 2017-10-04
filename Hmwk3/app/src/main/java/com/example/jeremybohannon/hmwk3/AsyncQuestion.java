fba4ddcfef35e5d5018e526c8eead8897411a1f8
Jeremy Bohannon <jeremybohannon@outlook.com>
2017-10-04 19:15:07 -0400

    connected async task to main activity

---
+++ Hmwk3/app/src/main/java/com/example/jeremybohannon/hmwk3/AsyncQuestion.java
@@ -0,0 +1,60 @@
+package com.example.jeremybohannon.hmwk3;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AsyncQuestion extends AsyncTask<String, Void, String> {
+    MainActivity activity;
+
+    public AsyncQuestion(MainActivity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        String result = null;
+        try {
+            URL url = new URL(strings[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                result = stringBuilder.toString();
+            }
+        } catch (Exception e) {
+            //Handle the exceptions
+        } finally {
+            //Close open connections and reader
+            if (connection != null) {
+                connection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+        activity.setQuestions(result);
+    }
+}
--- Hmwk3/app/src/main/java/com/example/jeremybohannon/hmwk3/MainActivity.java
+++ Hmwk3/app/src/main/java/com/example/jeremybohannon/hmwk3/MainActivity.java
@@ -1,18 +1,53 @@
 package com.example.jeremybohannon.hmwk3;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    ArrayList<Question> questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if(isConnected()) {
+            new AsyncQuestion(this).execute("http://dev.theappsdr.com/apis/trivia_json/trivia_text.php");
+        } else {
+            Toast.makeText(MainActivity.this, "No internet connection", Toast.LENGTH_SHORT).show();
+        }
 
 
 
+    }
 
+    public void setQuestions(String questions){
+        System.out.println(questions);
+        
+
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo == null || !networkInfo.isConnected() ||
+                (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                        && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+            return false;
+        }
+        return true;
     }
 }
