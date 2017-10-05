16c4d5d8b4f82744ed6cb017ce40f9e480313236
Elizabeth Thompson <ethomp54@uncc.edu>
2017-10-04 22:11:48 -0400

    updated trivia activity logic

--- Hmwk3/app/src/main/java/com/example/jeremybohannon/hmwk3/TriviaActivity.java
+++ Hmwk3/app/src/main/java/com/example/jeremybohannon/hmwk3/TriviaActivity.java
@@ -2,23 +2,31 @@ package com.example.jeremybohannon.hmwk3;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TriviaActivity extends AppCompatActivity {
 
     private ImageView questionImage;
-    private TextView questionNum, timeLeft, questionText, option1, option2, option3, option4;
+    private TextView questionNum, timeLeft, questionText;
+    private RadioGroup answerChoiceGroup;
     private Button quitBtn, nextBtn;
 
     private ArrayList<Question> questionList = new ArrayList<>();
     private int questionIndex;
+    private ArrayList<Integer> userAnswers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,33 +38,89 @@ public class TriviaActivity extends AppCompatActivity {
         questionNum   = (TextView) findViewById(R.id.questionNum);
         timeLeft      = (TextView) findViewById(R.id.timeLeft);
         questionText  = (TextView) findViewById(R.id.questionText);
-        option1       = (TextView) findViewById(R.id.optionOne);
-        option2       = (TextView) findViewById(R.id.optionTwo);
-        option3       = (TextView) findViewById(R.id.optionThree);
-        option4       = (TextView) findViewById(R.id.optionFour);
+
+        answerChoiceGroup = (RadioGroup) findViewById(R.id.answerChoices);
+        answerChoiceGroup.setOnCheckedChangeListener(answerChoiceChange);
 
         quitBtn = (Button) findViewById(R.id.quitBtn);
         nextBtn = (Button) findViewById(R.id.nextBtn);
+
+        quitBtn.setOnClickListener(quitOnClick);
+        nextBtn.setOnClickListener(nextOnClick);
 
         if (getIntent() != null && getIntent().getExtras() != null) {
             questionList = (ArrayList<Question>) getIntent().getExtras().getSerializable("EXTRA__QUESTIONS");
 
             questionIndex = 0;
-            Question currentQuestion = questionList.get(questionIndex);
-
-            Bitmap image = BitmapFactory.decodeFile(currentQuestion.getTriviaPhoto());
-            questionImage.setImageBitmap(image);
-
-            questionNum.setText("" + questionIndex + 1);
-            questionText.setText(currentQuestion.getTriviaQuestion());
-
-            String[] answerChoices = currentQuestion.getAnswerChoices();
-            option1.setText(answerChoices[0]);
-            option2.setText(answerChoices[1]);
-            option3.setText(answerChoices[2]);
-            option4.setText(answerChoices[3]);
-
-            int correctAnswer = currentQuestion.getAnswerIndex();
+            loadQuestionData(questionIndex);
         }
     }
+
+    private void loadQuestionData(int questionIndex) {
+        Question currentQuestion = questionList.get(questionIndex);
+
+        Bitmap image = BitmapFactory.decodeFile(currentQuestion.getTriviaPhoto());
+        questionImage.setImageBitmap(image);
+
+        questionNum.setText("" + questionIndex + 1);
+        questionText.setText(currentQuestion.getTriviaQuestion());
+
+        String[] answerChoices = currentQuestion.getAnswerChoices();
+
+        answerChoiceGroup.setOrientation(LinearLayout.VERTICAL);
+        answerChoiceGroup.removeAllViewsInLayout();
+
+        for (int i = 0; i < answerChoices.length; i++) {
+            if (answerChoices[i] != null) {
+                Log.d("debug", answerChoices[i]);
+
+                RadioButton answerChoice = new RadioButton(this);
+                answerChoice.setId(questionIndex + i);
+                answerChoice.setText(answerChoices[i]);
+                answerChoiceGroup.addView(answerChoice);
+            }
+        }
+    }
+
+    RadioGroup.OnCheckedChangeListener answerChoiceChange = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, @IdRes int id) {
+            RadioButton option = (RadioButton) findViewById(id);
+
+            Question currentQuestion = questionList.get(questionIndex);
+            String[] answerChoices = currentQuestion.getAnswerChoices();
+
+            for (int i = 0; i < answerChoices.length; i++) {
+                if (option.getText().equals(answerChoices[i])) {
+                    if (questionIndex > userAnswers.size() - 1) {
+                        userAnswers.add(i);
+                    } else {
+                        userAnswers.set(questionIndex, i);
+                    }
+                }
+            }
+        }
+    };
+
+    View.OnClickListener quitOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // TODO
+        }
+    };
+
+    View.OnClickListener nextOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (questionIndex == userAnswers.size() - 1) {
+                questionIndex++;
+
+                if (questionIndex < questionList.size()) {
+                    loadQuestionData(questionIndex);
+                }
+            } else {
+                Toast.makeText(TriviaActivity.this, "Please select an answer choice.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
