6e6ea30b7b9186e3ef7ece31cfc84cbd6666ef2f
jeremybohannon <jeremybohannon@outlook.com>
2017-10-30 19:04:51 -0400

    Stashing

---
+++ Group14_InClass07/app/src/main/java/com/example/android/group14_inclass07/Contact.java
@@ -0,0 +1,51 @@
+package com.example.android.group14_inclass07;
+
+/**
+ * Created by jeremybohannon on 10/30/17.
+ */
+
+class Contact {
+    private String name;
+    private String email;
+    private String department;
+    private int imageid;
+
+    public Contact(String name, String email, String department, int imageid) {
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.imageid = imageid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(int imageid) {
+        this.imageid = imageid;
+    }
+}
--- Group14_InClass07/app/src/main/java/com/example/android/group14_inclass07/MainActivity.java
+++ Group14_InClass07/app/src/main/java/com/example/android/group14_inclass07/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.android.group14_inclass07;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 
 
 }
