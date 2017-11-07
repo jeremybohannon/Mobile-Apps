645b0df27fcf3c4d89906b7e1bfb9492d69b13b8
jeremybohannon <jeremybohannon@outlook.com>
2017-11-06 20:57:57 -0500

    Updated msg view

--- Group14_InClass08/app/src/main/java/com/example/android/group14_inclass08/ChatActivity.java
+++ Group14_InClass08/app/src/main/java/com/example/android/group14_inclass08/ChatActivity.java
@@ -2,12 +2,50 @@ package com.example.android.group14_inclass08;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
+
+    TextView threadView;
+    EditText chatText;
+    ListView chatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        threadView = (TextView) findViewById(R.id.threadView);
+        chatText = (EditText) findViewById(R.id.chatText);
+        chatList = (ListView) findViewById(R.id.chatList);
+
+        final ArrayList<MessageObject> messages = new ArrayList<>();
+
+//        final MessageAdapter messageAdapter = new MessageAdapter(this, R.layout.message_view, MessageObject);
+//        chatList.setAdapter(messageAdapter);
+
+        chatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                int index = i;
+
+                MessageObject object = messages.get(i);
+
+
+                System.out.println("Object: " + object.getName());
+
+                messages.remove(i);
+
+//                messageAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
--- Group14_InClass08/app/src/main/java/com/example/android/group14_inclass08/MessageActivity.java
+++ Group14_InClass08/app/src/main/java/com/example/android/group14_inclass08/MessageActivity.java
@@ -2,12 +2,30 @@ package com.example.android.group14_inclass08;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
+
+    TextView nameView;
+    EditText threadText;
+    ListView threadList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        nameView = (TextView) findViewById(R.id.nameView);
+        threadText = (EditText) findViewById(R.id.threadText);
+        threadList = (ListView) findViewById(R.id.chatList);
+
+//        final MessageAdapter messageAdapter = new MessageAdapter(this, R.layout.message_view, MessageObject);
+//        threadList.setAdapter(messageAdapter);
+
+
+
+
     }
 }
