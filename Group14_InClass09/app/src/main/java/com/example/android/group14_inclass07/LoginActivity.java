9c107ede0b6a9323ce3efdea54707d9011730d99
jeremybohannon <jeremybohannon@outlook.com>
2017-11-13 18:56:20 -0500

    Login/sign up

---
+++ Group14_InClass09/app/src/main/java/com/example/android/group14_inclass07/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.group14_inclass07;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button loginBtn, signupBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("[LoginActivity | onCreate | loginBtn click] " + " Login btn clicked");
+                try {
+                    String loginEmail = email.getText().toString();
+                    String loginPassword = password.getText().toString();
+
+                    if(loginEmail.isEmpty() || loginPassword.isEmpty()) {
+                        System.out.println("[LoginActivity | onCreate | loginBtn click] " + " Empty credentials");
+
+                    } else {
+                        login(loginEmail, loginPassword);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        signupBtn = (Button) findViewById(R.id.signupBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("[LoginActivity | onCreate | signUp click] " + " Sign up btn clicked");
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void login(String email, String password) throws Exception {
+        System.out.println("[LoginActivity | login] " + " Login with email: " + email);
+
+
+
+
+    }
+}
---
+++ Group14_InClass09/app/src/main/java/com/example/android/group14_inclass07/SignUpActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.group14_inclass07;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText firstName, lastName, email, password, repeatPassword;
+    Button signUpBtn, cancelBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        setTitle("Sign Up");
+
+        firstName = (EditText) findViewById(R.id.firstNameText);
+        lastName = (EditText) findViewById(R.id.lastNameText);
+        email = (EditText) findViewById(R.id.emailText);
+        password = (EditText) findViewById(R.id.passwordText);
+        repeatPassword = (EditText) findViewById(R.id.repeatPasswordText);
+
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String firstNameVal, lastNameVal, emailVal, passwordVal, repeatPasswordVal;
+
+                    firstNameVal = firstName.getText().toString();
+                    lastNameVal = lastName.getText().toString();
+                    emailVal = email.getText().toString();
+                    passwordVal = password.getText().toString();
+                    repeatPasswordVal = repeatPassword.getText().toString();
+
+                    if (firstNameVal.isEmpty() || lastNameVal.isEmpty() || emailVal.isEmpty() || passwordVal.isEmpty() || repeatPasswordVal.isEmpty()) {
+                        Toast.makeText(SignUpActivity.this, "Please complete all fields", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if(passwordVal.length() < 6) {
+                            Toast.makeText(SignUpActivity.this, "Password must be 6 characters long", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (passwordVal.equals(repeatPasswordVal)) {
+                                signup(firstNameVal, lastNameVal, emailVal, passwordVal);
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        cancelBtn = (Button) findViewById(R.id.cancelBtn);
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void signup(String firstname, String lastname, String email, final String password) throws Exception {
+        
+    }
+}
